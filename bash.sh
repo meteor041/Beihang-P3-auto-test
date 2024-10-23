1b91109c267679896ec32178afe94b49c10d41b0
刘鑫宇 <23371510@buaa.edu.cn>
2024-10-23 14:15:53 +0800

    add autoGenerate.py

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
+python autoGenerate.py
 python autoRun.py test.asm machine_code.txt mips_memory.txt > mips_registers.txt
 python compareMemory.py mips_memory.txt logisim_memory.txt > result.txt
 python compareRegister.py mips_registers.txt logisim_registers.txt >> result.txt
