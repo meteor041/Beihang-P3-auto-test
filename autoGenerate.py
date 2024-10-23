67dfe440c12e0c2a14a78af95a83935282c12d81
刘鑫宇 <23371510@buaa.edu.cn>
2024-10-23 14:16:19 +0800

    fix bugs

--- autoGenerate.py
+++ autoGenerate.py
@@ -5,11 +5,11 @@ def rand_register():
     return '$' + str(random.randint(0, 25))
 
 
-def rand_signed_immediate_16():
-    return str(random.randint(-2 ** 15, 2 ** 15 - 1))
+def rand_lw_sw():
+    return str(random.randint(0, 500) * 4)
 
 
-def rand_unsigned_immediate_16():
+def rand_ori_lui():
     return str(random.randint(0, 2 ** 16 - 1))
 
 
@@ -19,7 +19,7 @@ def main():
     mark_cnt = 0
     ready_to_mark = []
     with open("test.asm", "w") as f:
-        f.write('.data\narr:.space 2000\n.text\n')
+        f.write('.text\n')
         f.write('loopx:\n'.replace('x', str(mark_cnt)))
         for _ in range(number):
             op = op_set[random.randint(0, len(op_set)-1)]
@@ -28,22 +28,18 @@ def main():
             elif op == 'sub':
                 f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' + rand_register() + '\n')
             elif op == 'ori':
-                f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' + rand_unsigned_immediate_16() + '\n')
+                f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' + rand_ori_lui() + '\n')
             elif op == 'lw':
-                f.write(op + ' ' + rand_register() + ',' + rand_signed_immediate_16() + '(' + rand_register() + ')\n')
+                f.write(op + ' ' + rand_register() + ',' + rand_lw_sw() + '(' + '$0' + ')\n')
             elif op == 'sw':
-                f.write(op + ' ' + rand_register() + ',' + rand_signed_immediate_16() + '(' + rand_register() + ')\n')
+                f.write(op + ' ' + rand_register() + ',' + rand_lw_sw() + '(' + '$0' + ')\n')
             elif op == 'beq':
-                if (random.randint(0,1) % 2 == 0):
-                    f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' +
-                        'loopx\n'.replace('x', str(random.randint(0,mark_cnt))) + '\n')
-                else:
-                    mark_cnt += 1
-                    f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' +
-                            'loopx\n'.replace('x', str(mark_cnt)) + '\n')
-                    ready_to_mark.append(mark_cnt)
+                mark_cnt += 1
+                f.write(op + ' ' + rand_register() + ',' + rand_register() + ',' +
+                        'loopx\n'.replace('x', str(mark_cnt)) + '\n')
+                ready_to_mark.append(mark_cnt)
             elif op == 'lui':
-                f.write(op + ' ' + rand_register() + ',' + rand_unsigned_immediate_16() + '\n')
+                f.write(op + ' ' + rand_register() + ',' + rand_ori_lui() + '\n')
             elif op == 'nop':
                 f.write(op + '\n')
             elif op == 'mark':
