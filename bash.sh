208642939fa0bee6df7246836d1375505f83f9c1
刘鑫宇 <23371510@buaa.edu.cn>
2024-10-27 19:25:52 +0800

    初始测试文件

--- bash.sh
+++ bash.sh
@@ -1,5 +1,2 @@
-python autoGenerate.py
-python autoRun.py ../docs/test.asm ../docs/machine_code.txt ../docs/mips_memory.txt > ../docs/mips_registers.txt
-python logisimRun.py ../your_cpu/cpu_wrong.circ
-python compareMemory.py mips_memory.txt logisim_memory.txt > result.txt
-python compareRegister.py mips_registers.txt logisim_registers.txt >> result.txt
+cd src
+python gui.py
